5614cb5ae0621ed747600671c05c96aeb096e1ee
jahyeonjo <amirgeraldgilbuena@gmail.com>
2026-02-12 09:27:22 +0800

    initialize variables and call variables

--- lib/practice/01_variables_basics.dart
+++ lib/practice/01_variables_basics.dart
@@ -18,9 +18,19 @@
 void main() {
   // 1. Variable Declarations
   print("This is an introduction to variable declarations in Dart.");
+
   var name = "Shaina"; // Type inferred as String
+
+  // name = 11; // Reassigning an int to a String variable (will cause an error) - Don't do this.
 
   name = "Claris"; // Reassigning a new value
 
+  String city = "Tomasa"; // Explicitly declaring a String variable
+
+  int age = 22; // Declaring an integer variable
+  double height = 5.3; // Declaring a double variable
+  bool isPretty = true; // Declaring a boolean variable
+
   print(name);
+  print("${name} is ${age} years old, she lives in ${city}, and she is ${height} feet tall. And she's very Pretty. Right? Yup, ${isPretty}");
 }
