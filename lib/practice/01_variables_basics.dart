13d623966315bbba5066ff65aed01bce296cdce5
jahyeonjo <amirgeraldgilbuena@gmail.com>
2026-02-12 10:12:28 +0800

    add additional type inference examples

--- lib/practice/01_variables_basics.dart
+++ lib/practice/01_variables_basics.dart
@@ -24,7 +24,7 @@ void main() {
   var name = "Shaina"; // Dart infers as String
   print("✓ Type Inference: name = '$name'");
 
-  name = "Claris"; // Can reassign same type
+  name = "Shaina Claris Barreto Morales"; // Can reassign same type
   print("✓ Reassigned: name = '$name'\n");
 
   // ========== EXPLICIT TYPES ==========
@@ -44,4 +44,28 @@ void main() {
   print("Summary: $name is $age years old, lives in $city, "
       "is $height feet tall, and is pretty: $isPretty");
   print("====================================");
+
+  // ========== TYPE INFERENCE ============
+  print("====================================");
+  print("  TYPE INFERENCE & DECLARATION PRACTICE ");
+  print("====================================\n");
+
+  // ========== INFERENCE TYPES ==========
+  var name0 = "Amir Gerald Giducos Gilbuena"; // Inferred as String
+  var city0 = "California"; // Inferred as String
+  var age0 = 21; // Inferred as int
+  var height0 = 5.8; // Inferred as double
+  var isHandsome = true; // Inferred as bool
+
+  print("✓ Type Inference:");
+  print(" • Name: $name0");
+  print(" • City: $city0");
+  print(" • Age: $age0");
+  print(" • Height: $height0 ft");
+  print(" • Handsome: $isHandsome\n");
+
+  // ========== DISPLAY ALL INFO ==========
+  print("====================================");
+  print("Summary: $name0 is $age0 years old, and lives in $city0, "
+      "$name0 is $height0 feet tall, and is handsome: $isHandsome");
 }
